76d54ef4e11e678b7a0d3b4b886d8e442850b760
VovaTaranik <122770954+VovaTaranik@users.noreply.github.com>
2023-01-16 13:16:52 +0200

    Update 3.py

--- MODUULI_2/3.py
+++ MODUULI_2/3.py
@@ -1,5 +1,5 @@
-kanta = int(input("Kanta : "))
-korkeus = int(input("Korkeus : "))
+kanta = float(input("Kanta : "))
+korkeus = float(input("Korkeus : "))
 pinta_ala = kanta*korkeus
 piiri=2*(kanta+korkeus)
 print("Suorakulmion pinta-ala : ",pinta_ala)
